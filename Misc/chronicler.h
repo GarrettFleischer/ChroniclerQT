8b53c54bc0c07c8e9c77d299033d641c561543d6
GarrettFleischer <garrett.fleischer@oit.edu>
2016-09-09 21:34:02 -0700

    Updated program version.

--- Misc/chronicler.h
+++ Misc/chronicler.h
@@ -119,7 +119,7 @@ namespace Chronicler
     // Do NOT instantiate this struct, use shared() singleton access.
     struct SharedInstances
     {
-        CVersion ProgramVersion = CVersion("0.9.10.0");
+        CVersion ProgramVersion = CVersion("0.9.12.2");
 
         CMainWindow *mainWindow;
 
