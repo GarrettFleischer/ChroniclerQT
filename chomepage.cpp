9fec4a9d076ad01066ffa03229adf97c2f321d1f
GarrettFleischer <garrett.fleischer@oit.edu>
2017-01-29 20:14:54 -0800

    Fixed updater for Mac version. Added icon for Mac version. Fixed crash when closing without opening any projects first.

--- chomepage.cpp
+++ chomepage.cpp
@@ -56,7 +56,7 @@ CHomepage::CHomepage(QWidget *parent)
 #endif
 #ifdef Q_OS_OSX
     m_os = Mac;
-    m_executable = "/Chronicler-Next";
+    m_executable = "/Chronicler-Next.app";
 #endif
 #ifdef Q_OS_LINUX
     m_os = Linux;
@@ -291,11 +291,17 @@ void CHomepage::FinishInstallingUpdates()
             shared().mainWindow->close();
             QDesktopServices::openUrl(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/Chronicler_Updater.exe"));
         }
-        else if(m_os == Linux || m_os == Mac)
+        else if(m_os == Linux)
         {
             QProcess *process = new QProcess();
             connect(process, SIGNAL(finished(int)), shared().mainWindow, SLOT(close()));
             process->start(QCoreApplication::applicationDirPath() + "/Chronicler_Updater");
+        }
+        else if(m_os == Mac)
+        {
+            QProcess *process = new QProcess();
+            connect(process, SIGNAL(finished(int)), shared().mainWindow, SLOT(close()));
+            process->start(QCoreApplication::applicationDirPath() + "/Chronicler_Updater.app");
         }
     }
 }
